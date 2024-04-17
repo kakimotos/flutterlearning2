a1365b7676bf0575650963960bbaa2aa6ddad2aa
kakimotos <ikaruga.hajme@gmail.com>
2024-04-17 13:53:36 +0900

    14章ClipRRectまで

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
 
   @override
@@ -46,8 +44,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: const Text('こんにちは'),
+      body:Center(
+        child: Row(
+          children: [
+            // myDecoratedBox(),
+            Opacity(
+              opacity: 0.2,
+              child: myDecoratedBox(greeting: 'さようなら')
+            ),
+            ClipOval(
+              child: Image.asset(
+                'images/animal5.png',
+                width: 150,
+                height: 100,
+                fit: BoxFit.cover
+              )
+            ),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                clipBehavior: Clip.hardEdge,
+                child: Image.asset(
+                    'images/animal5.png',
+                    width: 150,
+                    height: 100,
+                    fit: BoxFit.cover
+                )
+            ),
+
+          ],
+        ),
+      )
        // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget myDecoratedBox({ String greeting = 'こんにちは'}) {
+  return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.purple.shade50,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        // edge…端っこ、角　Insets…「入れ込む」「差し込む」Inserts
+        child: Text(greeting),
+      )
+  );
 }
